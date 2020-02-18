5eefe556068e238e623de8bd17f613c0caab5555
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-18 19:31:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,10 @@
 class Dog
-  
-  def initialize(dog_name, breed_name)
-    @dog_name = dog_name
-    @breed_name = breed_name
-  end
-  
-  def dog=(dog_name_and_breed)
-    dog_name, breed_name = dog_name_and_breed.split
-    @dog_name = dog_name
-    @breed_name = breed_name
-  end
-  
-  def dog 
-    "#{@dog_name} #{@breed_name}".strip
-  end
+
+def name(dog_name)
+  @name = dog_name
 end
+
+
+  
+end
+
